00a85eff9784e2ba22b930c79e2670f8f4666b93
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-04-25 00:55:39 +0900

    2025-04-24

--- abc201-300/abc247/abc247_b/main.cpp
+++ abc201-300/abc247/abc247_b/main.cpp
@@ -4,5 +4,43 @@ using ll = long long;
 
 int main()
 {
+    int n;
+    cin >> n;
+    vector<pair<string, string>> name(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name[i].first >> name[i].second;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        bool flg1 = true, flg2 = true;
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                if (name[i].first == name[j].first || name[i].first == name[j].second)
+                {
+                    flg1 = false;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                if (name[i].second == name[j].first || name[i].second == name[j].second)
+                {
+                    flg2 = false;
+                }
+            }
+        }
+        if (!flg1 && !flg2)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
     return 0;
 }
