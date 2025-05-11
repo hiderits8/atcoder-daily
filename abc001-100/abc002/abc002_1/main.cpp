1c5eee84ce46d193031828b38bfedce823375872
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-05-11 23:54:22 +0900

    2025-05-11

---
+++ abc001-100/abc002/abc002_1/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    ll res = a > b ? a : b;
+
+    cout << res << endl;
+
+    return 0;
+}
