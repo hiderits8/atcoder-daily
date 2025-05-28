cb53e2b9a5bffa88b929771e50722fe961142a9e
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-05-28 22:19:13 +0900

    2025-05-27

---
+++ abc201-300/abc288/abc288_c/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n, vector<int>());
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    vector<bool> used(n);
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (!used[i])
+        {
+            s++;
+            queue<int> que;
+            que.push(i);
+            while (!que.empty())
+            {
+                int q = que.front();
+                que.pop();
+                for (auto v : graph[q])
+                {
+                    if (!used[v])
+                    {
+                        used[v] = true;
+                        que.push(v);
+                    }
+                }
+            }
+        }
+    }
+    cout << m - (n - s) << endl;
+
+    return 0;
+}
