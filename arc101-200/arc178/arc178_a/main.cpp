3468dd4d11da5d928b0768b3344863560fea3f66
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-06-02 00:05:02 +0900

    2025-05-30

--- arc101-200/arc178/arc178_a/main.cpp
+++ arc101-200/arc178/arc178_a/main.cpp
@@ -6,24 +6,26 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-    vector<int> ban(n);
+    vector<int> arr(n);
     for (int i = 0; i < m; i++)
     {
         int a;
         cin >> a;
-        ban[a - 1]++;
+        arr[a - 1]++;
     }
 
-    if (ban[0] || ban[n - 1])
+    if (arr[0] || arr[n - 1])
     {
         cout << "-1\n";
         return 0;
     }
+
     vector<int> p(n);
     iota(p.begin(), p.end(), 1);
+
     for (int i = 1; i < n - 1; i++)
     {
-        if (ban[i])
+        if (arr[i])
         {
             swap(p[i], p[i + 1]);
         }
@@ -31,7 +33,15 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cout << p[i] << (i == n - 1 ? "\n" : " ");
+        cout << p[i];
+        if (i == n - 1)
+        {
+            cout << endl;
+        }
+        else
+        {
+            cout << " ";
+        }
     }
     return 0;
 }
