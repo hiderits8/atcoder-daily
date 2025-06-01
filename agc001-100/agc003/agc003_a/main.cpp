997f61b02005fb4d90fd6fb363fb20b45cbd6b77
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-06-01 23:47:32 +0900

    2025-05-31

---
+++ agc001-100/agc003/agc003_a/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
+    }
+
+    if (((mp['N'] && mp['S']) || (!mp['N'] && !mp['S'])) && ((mp['W'] && mp['E']) || (!mp['W'] && !mp['E'])))
+    {
+        cout << "Yes" << endl;
+        return 0;
+    }
+    cout << "No" << endl;
+    return 0;
+}
