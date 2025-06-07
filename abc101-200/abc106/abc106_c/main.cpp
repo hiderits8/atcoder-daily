3a9b9d77f0ade0901a8b1f81756c164a3ad3a6a8
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-06-07 15:23:02 +0900

    2025-06-06

---
+++ abc101-200/abc106/abc106_c/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    string s;
+    ll k;
+    cin >> s >> k;
+
+    if (s[0] != '1')
+    {
+        cout << s[0] << endl;
+        return 0;
+    }
+
+    int idx1 = 0;
+    for (int i = 1; i < s.size(); ++i)
+    {
+        if (s[i] == '1')
+        {
+            idx1 = i;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (idx1 + 1 <= k)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << s[idx1 + 1] << endl;
+    }
+
+    return 0;
+}
