a0a693e6a753cbe09f73669e762c44ecd17b0cd6
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-06-12 00:01:03 +0900

    2025-06-11

---
+++ other-contest/tenka1-2015-qualb/tenka1_2015_qualB_a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    vector<int> a(20);
+    a[0] = 100;
+    a[1] = 100;
+    a[2] = 200;
+
+    for (int i = 3; i < 20; ++i)
+    {
+        a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+    }
+
+    cout << a[19] << endl;
+    return 0;
+}
