be5bac0eb2ee80249bfe24afc44a84f09758c659
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-08-15 20:47:04 +0900

    2025-08-15

--- abc401-500/abc410/abc410_d/main.cpp
+++ abc401-500/abc410/abc410_d/main.cpp
@@ -2,8 +2,58 @@
 using namespace std;
 using ll = long long;
 
+struct Edge
+{
+    int to, w;
+    Edge(int to, int w) : to(to), w(w) {}
+};
+
 int main()
 {
+    int n, m;
+    cin >> n >> m;
+    int W = 1 << 10;
 
+    vector<vector<Edge>> g(n);
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        a--;
+        b--;
+        g[a].emplace_back(b, w);
+    }
+
+    vector<bool> visited(n * W);
+    queue<int> q;
+    auto push = [&](int v, int x)
+    {
+        int vid = v * W + x;
+        if (visited[vid])
+            return;
+        visited[vid] = true;
+        q.push(vid);
+    };
+    push(0, 0);
+    while (!q.empty())
+    {
+        int vid = q.front();
+        q.pop();
+        int v = vid / W, x = vid % W;
+        for (auto e : g[v])
+        {
+            push(e.to, x ^ e.w);
+        }
+    }
+
+    for (int x = 0; x < W; ++x)
+    {
+        if (visited[(n - 1) * W + x])
+        {
+            cout << x << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
     return 0;
 }
