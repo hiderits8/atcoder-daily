aa200813ea44879e7e4759cf7609f761a6b7ba79
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-10-15 07:33:56 +0900

    2025-10-15

---
+++ abc101-200/abc137/abc137_c/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    map<string, int> mp;
+    rep(i, n)
+    {
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        mp[s]++;
+    }
+
+    ll ans = 0;
+    for (auto [s, c] : mp)
+    {
+        ans += c * (c - 1) / 2;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
