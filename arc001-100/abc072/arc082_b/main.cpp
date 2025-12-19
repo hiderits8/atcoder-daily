a95f842d5df53f1b315126737ea64bf69fc6c266
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-12-20 00:08:49 +0900

    2025-12-19

---
+++ arc001-100/abc072/arc082_b/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    rep(i, n) cin >> a[i];
+
+    int ans = 0;
+    rep(i, n - 1)
+    {
+        if (a[i] == i + 1)
+        {
+            swap(a[i], a[i + 1]);
+            ans++;
+        }
+    }
+    if (a[n - 1] == n)
+        ans++;
+    cout << ans << endl;
+    return 0;
+}
