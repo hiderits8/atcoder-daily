424bb883cdacb4bfca338a283263fdfb6571ec6e
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2026-01-29 11:30:02 +0900

    2026-01-27

--- abc201-300/abc258/abc258_d/main.cpp
+++ abc201-300/abc258/abc258_d/main.cpp
@@ -18,5 +18,22 @@ using ll = long long;
 
 int main()
 {
+    int n;
+    ll x;
+    cin >> n >> x;
+
+    vector<ll> a(n), b(n), sum(n + 1, 0);
+    rep(i, n)
+    {
+        cin >> a[i] >> b[i];
+        sum[i + 1] = sum[i] + a[i] + b[i];
+    }
+    ll ans = LLONG_MAX;
+    for (ll i = 0; i < n && i < x; i++)
+    {
+        ll tmp = sum[i + 1] + b[i] * (x - i - 1);
+        ans = min(ans, tmp);
+    }
+    cout << ans << endl;
     return 0;
 }
