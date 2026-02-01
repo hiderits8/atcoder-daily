3dc7b4c54e53f806fdc0d21b8fa3f6ce94fd0d9f
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2026-02-02 03:32:58 +0900

    2026-02-02

---
+++ abc001-100/abc070/abc070_c/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> t(n);
+    rep(i, n) cin >> t[i];
+    ll ans = 1;
+    rep(i, n) ans = lcm(ans, t[i]);
+    cout << ans << endl;
+    return 0;
+}
