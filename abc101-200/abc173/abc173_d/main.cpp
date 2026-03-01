a85d1edb84f569521bc89ce8f413aa65e0753e9d
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2026-03-01 15:09:00 +0900

    2026-02-25

---
+++ abc101-200/abc173/abc173_d/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+    sort(a.rbegin(), a.rend());
+    ll ans = 0;
+    rep(i, n - 1)
+    {
+        ans += a[(i + 1) / 2];
+    }
+    cout << ans << endl;
+    return 0;
+}
